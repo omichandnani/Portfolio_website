3cd5cc3893cea2b88aff14276361cec3c74e17b8
omichandnani <chan8860@mylaurier.ca>
2021-07-12 16:50:31 -0400

    adding portfolio changes

---
+++ build/precache-manifest.10f415cd6f552c39390c8e65bc911456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2252c17d14029d603b11a46eceb804",
+    "url": "/Portfolio_website/index.html"
+  },
+  {
+    "revision": "3d5aae12ac27f298a781",
+    "url": "/Portfolio_website/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "f261635051f053f57b7b",
+    "url": "/Portfolio_website/static/css/main.135e6a3f.chunk.css"
+  },
+  {
+    "revision": "3d5aae12ac27f298a781",
+    "url": "/Portfolio_website/static/js/2.9d42db28.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio_website/static/js/2.9d42db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f261635051f053f57b7b",
+    "url": "/Portfolio_website/static/js/main.6c08689a.chunk.js"
+  },
+  {
+    "revision": "41fe58bb5fa2246e9120",
+    "url": "/Portfolio_website/static/js/runtime-main.c4e080f0.js"
+  }
+]);
